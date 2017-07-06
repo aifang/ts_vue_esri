92667e2131a8e9587138be9d3cb8d5099e515689
aifang <aifanghang@github.com>
2017-07-06 20:44:53 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import * as Vue from 'vue';
+import _ from 'lodash';
+import esriLoader from 'esri-loader';
+import Map from "esri/map";
+
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/main.ts',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            // {
+            //     test: /\.html$/,
+            //     loader: 'html-loader',
+            //     exclude: /node_modules/
+            // },
+            // {
+            //     test: /\.css$/,
+            //     loader: 'style-loader!css-loader'
+            // },
+            // {
+            //     test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+            //     loader: 'file-loader'
+            // },
+        ]
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.ts', '.tsx', '.js', 'html'],
+        alias: {
+            'vue$': 'vue/dist/vue.common.js',
+	        'vue-router$': 'vue-router/dist/vue-router.common.js'
+        }
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
+}
+
+/* 生产环境的配置 */
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }),
+        new webpack.BannerPlugin('This file is created by FH')
+    ])
+}
