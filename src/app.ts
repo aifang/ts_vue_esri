ccbd23e5ce8c38a607e53983df5df5a990f902ad
aifang <aifanghang@github.com>
2017-07-18 17:46:35 +0800

    clear map

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import * as Vue from 'vue';
 import * as VueRouter from 'vue-router'
 //import _ from 'lodash';
-import * as ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css';
+// import * as ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-default/index.css';
 import routes from './routes'
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -20,13 +20,3 @@ const app = new Vue({
         return { visible: false }
     }
 });
-
-
-
-// esriLoader.dojoRequire(["esri/map"], (Map) => {
-//     let map = new Map("map", {
-//         center: [-118, 34.50],
-//         zoom: 8,
-//         basemap: "dark-gray"
-//     });
-// });
