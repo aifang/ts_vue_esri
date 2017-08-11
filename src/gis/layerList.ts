8d8cac4ac09122c166e7b4e8587311e27522f14e
aifang <aifanghang@github.com>
2017-08-11 14:54:07 +0800

    FindResult

--- src/gis/layerList.ts
+++ src/gis/layerList.ts
@@ -2,11 +2,13 @@
 import ArcGISDynamicMapServiceLayer = require("esri/layers/ArcGISDynamicMapServiceLayer")
 import FindParameters = require("esri/tasks/FindParameters")
 import FindTask = require("esri/tasks/FindTask")
+import FindResult = require("esri/tasks/FindResult")
 import Extent = require("esri/geometry/Extent")
 import SimpleLineSymbol = require("esri/symbols/SimpleLineSymbol")
 import SimpleFillSymbol = require("esri/symbols/SimpleFillSymbol")
 import Color = require("esri/Color")
 import Graphic = require("esri/graphic")
+import on = require("dojo/on")
 
 function seacrchFeature(a, b, lyrServer: ArcGISDynamicMapServiceLayer) {
     var lyrIds = lyrServer.layerInfos.map((info) => { return info.id; });
@@ -18,7 +20,7 @@ function seacrchFeature(a, b, lyrServer: ArcGISDynamicMapServiceLayer) {
     var findTask = new FindTask(lyrServer.url);
     var promise = findTask.execute(params).then(showFindResult)
 }
-function showFindResult(queryResult) {
+function showFindResult(queryResult:FindResult[]) {
     if (queryResult.length == 0) {
         alert("没有该元素.");
         return;
