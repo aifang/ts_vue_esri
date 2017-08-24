04157f7d87b136ad2ce1b9c6e899d0dd10744532
xu1990912zheng@hotmail.com <xu2011zheng@hotmail.com>
2017-08-24 18:25:01 +0800

    多人提交测试

--- src/component/map/map.vue
+++ src/component/map/map.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
         <search :layer="layer"></search>
-        <el-button @click="onClick">{{message}}</el-button>
-        <div id="map"> </div>
+        <tools></tools>
+        <div id="map" class="big-map"></div>
     </div>
 </template>
+<style>
+    .big-map{width:100%;height:100%;position: absolute;top:0;left:0;z-index: -1;}
+</style>
 <script lang="ts">
 import Map from "./map";
 export default Map;
